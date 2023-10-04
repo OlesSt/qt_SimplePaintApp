8bec506bd0d47cbbe163beca92c266c3746cc98a
Oles <oles1stepanov@gmail.com>
2023-10-04 13:56:45 +0300

    Implement signal and slots

--- paint.cpp
+++ paint.cpp
@@ -1,81 +1,90 @@
 #include "paint.h"
 #include "./ui_paint.h"
 
-Paint::Paint(QWidget *parent) : QMainWindow(parent)
+#include<QPainter>
+#include<QFileDialog>
+
+Paint::Paint(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
+    ui->setupUi(this);
 
     connect(ui->actionSave, SIGNAL(triggered()), this, SLOT(actionSave()));
     connect(ui->actionClear, SIGNAL(triggered()), this, SLOT(actionClear()));
+    connect(ui->action3px, SIGNAL(triggered()), this, SLOT(action3px()));
+    connect(ui->action5px, SIGNAL(triggered()), this, SLOT(action5px()));
+    connect(ui->action7px, SIGNAL(triggered()), this, SLOT(action7px()));
+    connect(ui->action9px, SIGNAL(triggered()), this, SLOT(action9px()));
+    connect(ui->actionBlack, SIGNAL(triggered()), this, SLOT(actionBlack()));
+    connect(ui->actionGreen, SIGNAL(triggered()), this, SLOT(actionGreen()));
+    connect(ui->actionRed, SIGNAL(triggered()), this, SLOT(actionRed()));
+    connect(ui->actionYellow, SIGNAL(triggered()), this, SLOT(actionYellow()));
+    connect(ui->actionErase, SIGNAL(triggered()), this, SLOT(actionErase()));
 
-    ui = new Ui::MainWindow();
-    ui->setupUi(this);
-
-    p = new Painter();
-    p->setImage(QImage(this->size(), QImage::Format_RGB32));
-    p->setDrawing(false);
-    p->setBrushColor(Qt::black);
-    p->setBrushSize(1);
-
+    image = QImage(this->size(), QImage::Format_RGB32);
+    image.fill(Qt::white);
+    drawing=false;
+    brushColor = Qt::black;
+    brushSize = 2;
 }
 
 Paint::~Paint()
 {
     delete ui;
-    delete p;
 }
 
 void Paint::mousePressEvent(QMouseEvent *event)
 {
-    if(event->button() == Qt::LeftButton)
-    {
-        p->setDrawing(true);
-        p->setLastPoint(event->pos());
+    if(event->button() == Qt::LeftButton) {
+        drawing=true;
+        lastPoint=event->pos();
     }
 }
 
 void Paint::mouseMoveEvent(QMouseEvent *event)
 {
-    if((event->buttons() & Qt::LeftButton) && p->getDrawing())
-    {
-        QImage img = p->getImage();
-        QPainter painter(&img);
-        painter.setPen(QPen(p->getBrushColor(), p->getBrushSize(), Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-        painter.drawLine(p->getLastPoint(), event->pos());
-        p->setLastPoint(event->pos());
+    if((event->buttons() & Qt::LeftButton) && drawing) {
+        QPainter painter(&image);
+        painter.setPen(QPen(brushColor, brushSize, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+        painter.drawLine(lastPoint, event->pos());
+        lastPoint = event->pos();
         this->update();
     }
 }
 
 void Paint::mouseReleaseEvent(QMouseEvent *event)
 {
-    if(event->button() == Qt::LeftButton)
-    {
-        p->setDrawing(false);
+    if(event->button() == Qt::LeftButton) {
+        drawing=false;
     }
 }
 
 void Paint::paintEvent(QPaintEvent *event)
 {
-    QImage img = p->getImage();
     QPainter canvasPainter(this);
-    canvasPainter.drawImage(this->rect(), img, img.rect());
+    canvasPainter.drawImage(this->rect(), image, image.rect());
 }
 
 void Paint::actionSave()
 {
-    QString filePath = QFileDialog::getSaveFileName(this,
-                                                    "Save Image",
-                                                    "",
-                                                    "PNG(*.png);;"
-                                                    "JPEG(*.jpg *.jpeg);;"
-                                                    "All files(*.*)");
+    QString filePath = QFileDialog::getSaveFileName(this, "Save Image", "", "PNG(*.png);;JPEG(*.jpg *.jpeg);;All files(*.*)");
     if(filePath == "") { return;}
-    p->getImage().save(filePath);
+    image.save(filePath);
 }
 
 void Paint::actionClear()
 {
-    QImage img = p->getImage();
-    img.fill(Qt::white);
-    p->setImage(img);
+    image.fill(Qt::white);
+    this->update();
 }
+
+void Paint::action3px()     {brushSize=3;}
+void Paint::action5px()     {brushSize=5;}
+void Paint::action7px()     {brushSize=7;}
+void Paint::action9px()     {brushSize=9;}
+void Paint::actionBlack()   {brushColor=Qt::black;}
+void Paint::actionErase()   {brushColor=Qt::white;}
+void Paint::actionRed()     {brushColor=Qt::red;}
+void Paint::actionGreen()   {brushColor=Qt::green;}
+void Paint::actionYellow()  {brushColor=Qt::yellow;}
--- paint.h
+++ paint.h
@@ -4,7 +4,7 @@
 #include <QMainWindow>
 #include <QMouseEvent>
 #include <QFileDialog>
-#include "painter.h"
+#include <QObject>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {class MainWindow;}
@@ -16,32 +16,32 @@ class Paint : public QMainWindow
 public:
     Paint(QWidget *parent = nullptr);
     ~Paint();
-
     virtual void mousePressEvent(QMouseEvent *event);
     virtual void mouseMoveEvent(QMouseEvent *event);
     virtual void mouseReleaseEvent(QMouseEvent *event);
     virtual void paintEvent(QPaintEvent *event);
 
+
+private slots:
     void actionSave();
     void actionClear();
-
     void action3px();
     void action5px();
     void action7px();
     void action9px();
-
     void actionBlack();
+    void actionErase();
     void actionRed();
     void actionGreen();
     void actionYellow();
-    void actionErase();
-
-
 
 private:
     Ui::MainWindow *ui;
-    Painter *p;
-
+    QImage image;
+    bool drawing;
+    QPoint lastPoint;
+    int brushSize;
+    QColor brushColor;
 };
 
 #endif // PAINT_H
